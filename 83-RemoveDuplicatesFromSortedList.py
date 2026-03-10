d8ef54c17cb1a025526dde6a19c9dca5cc111f7a
GBinh02 <giagia8203@gmail,com>
2026-03-10 14:30:55 +0700

    push code

---
+++ 83-RemoveDuplicatesFromSortedList.py
@@ -0,0 +1,15 @@
+class Solution:
+    def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+
+        current = head
+
+        while current and current.next:
+
+            if current.val == current.next.val:
+                # Bỏ node trùng
+                current.next = current.next.next
+            else:
+                current = current.next
+
+        return head
+        
